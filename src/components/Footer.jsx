bd953ddda529f8d401b0ccda03774f44ea4496e4
Pedro Miguel <pmig.magalhaes@gmail.com>
2025-10-03 14:06:23 +0200

    Finish responsive design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,14 +19,15 @@ export default function Footer({ darkMode }) {
           TMBd API
         </a>
       </span>
+      <div className={styles.socials}>
       <span className={styles.pedro}>
         <a
           href="https://pedromagalhaes.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
-        >made by Pedro</a>
+        >made by Pedro</a> |
       </span>
-      <div className={styles.socials}>
+      
         <a
           href="https://github.com/Pedrocas74"
           target="_blank"
